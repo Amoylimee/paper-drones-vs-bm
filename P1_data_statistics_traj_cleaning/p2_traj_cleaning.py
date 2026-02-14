c6698ab647f3cd4fc03324e01a25f04321d1d67d
Jiajing <jchenhl@connect.ust.hk>
2026-02-14 19:35:46 +0800

    Simplify the cleaning logic

--- P1_data_statistics_traj_cleaning/p2_traj_cleaning.py
+++ P1_data_statistics_traj_cleaning/p2_traj_cleaning.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
         )
 
         steps = [
-            "basic",
+            # "basic",
             "tbd_drift",
             # "bidirectional_speed_drift",
             "speed_anomalies",
